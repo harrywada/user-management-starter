1a2ecdfda2e8ebda235cb734f380790c1ab9f53d
Harry Wada <harrywada@gmail.com>
2016-09-15 19:13:32 -0500

    added basic form to add user

--- client/js/views/UsersCollectionView.js
+++ client/js/views/UsersCollectionView.js
@@ -3,9 +3,57 @@ const Backbone = require("backbone");
 
 const UsersCollection = require("../collections/UsersCollection");
 const UserItemView = require("../views/UserItemView");
+const UserModel = require("../models/UserModel");
 
 const UsersCollectionView = Backbone.View.extend({
-  el: '<ul class="user-list"></ul>',
+  el: `
+    <div>
+      <div class="user-form">
+        <form>
+          <label for="name">Name: </label><br>
+          <input type="text" name="name" /><br>
+
+          <label for="email">Email: </label><br>
+          <input type="text" name="email" /><br>
+
+          <label for="bio">Bio: </label><br>
+          <input type="text" name="bio" /><br>
+
+          <label for="image">Image URL: </label><br>
+          <input type="text" name="imageURL" /><br>
+
+          <input type="submit" name="submit" value="add user" />
+        </form>
+      </div>
+      <ul class="user-list"></ul>
+    </div>
+  `,
+
+  events: {
+    "submit form": "handleFormSubmit"
+  },
+
+  handleFormSubmit(e){
+    const form = $(e.target);
+
+    const newUser = new UserModel({
+      name: form.find("input[name=\"name\"]").val(),
+      email: form.find("input[name=\"email\"]").val(),
+      bio: form.find("input[name=\"bio\"]").val(),
+      imageURL: form.find("input[name=\"imageURL\"]").val()
+    });
+
+    console.log(newUser);
+
+    this.collection.add(newUser).save();
+
+    form.find("input[name=\"name\"]").val("");
+    form.find("input[name=\"email\"]").val("");
+    form.find("input[name=\"bio\"]").val("");
+    form.find("input[name=\"imageURL\"]").val("");
+
+    e.preventDefault();
+  },
 
   initialize(){
     this.collection = new UsersCollection();
@@ -13,11 +61,12 @@ const UsersCollectionView = Backbone.View.extend({
   },
 
   render(){
-    this.$el.html("");
+    console.log("render");
+    this.$el.find("ul").html("");
 
     this.collection.each(user => {
       const view = new UserItemView({model: user});
-      this.$el.append(view.render().el);
+      this.$el.find("ul").append(view.render().el);
     });
     return this;
   }
--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -163,9 +163,57 @@ const Backbone = require("backbone");
 
 const UsersCollection = require("../collections/UsersCollection");
 const UserItemView = require("../views/UserItemView");
+const UserModel = require("../models/UserModel");
 
 const UsersCollectionView = Backbone.View.extend({
-  el: '<ul class="user-list"></ul>',
+  el: `
+    <div>
+      <div class="user-form">
+        <form>
+          <label for="name">Name: </label><br>
+          <input type="text" name="name" /><br>
+
+          <label for="email">Email: </label><br>
+          <input type="text" name="email" /><br>
+
+          <label for="bio">Bio: </label><br>
+          <input type="text" name="bio" /><br>
+
+          <label for="image">Image URL: </label><br>
+          <input type="text" name="imageURL" /><br>
+
+          <input type="submit" name="submit" value="add user" />
+        </form>
+      </div>
+      <ul class="user-list"></ul>
+    </div>
+  `,
+
+  events: {
+    "submit form": "handleFormSubmit"
+  },
+
+  handleFormSubmit(e) {
+    const form = $(e.target);
+
+    const newUser = new UserModel({
+      name: form.find("input[name=\"name\"]").val(),
+      email: form.find("input[name=\"email\"]").val(),
+      bio: form.find("input[name=\"bio\"]").val(),
+      imageURL: form.find("input[name=\"imageURL\"]").val()
+    });
+
+    console.log(newUser);
+
+    this.collection.add(newUser).save();
+
+    form.find("input[name=\"name\"]").val("");
+    form.find("input[name=\"email\"]").val("");
+    form.find("input[name=\"bio\"]").val("");
+    form.find("input[name=\"imageURL\"]").val("");
+
+    e.preventDefault();
+  },
 
   initialize() {
     this.collection = new UsersCollection();
@@ -173,11 +221,12 @@ const UsersCollectionView = Backbone.View.extend({
   },
 
   render() {
-    this.$el.html("");
+    console.log("render");
+    this.$el.find("ul").html("");
 
     this.collection.each(user => {
       const view = new UserItemView({ model: user });
-      this.$el.append(view.render().el);
+      this.$el.find("ul").append(view.render().el);
     });
     return this;
   }
@@ -185,7 +234,7 @@ const UsersCollectionView = Backbone.View.extend({
 
 module.exports = UsersCollectionView;
 
-},{"../collections/UsersCollection":2,"../views/UserItemView":5,"backbone":8,"lodash":10}],8:[function(require,module,exports){
+},{"../collections/UsersCollection":2,"../models/UserModel":3,"../views/UserItemView":5,"backbone":8,"lodash":10}],8:[function(require,module,exports){
 (function (global){
 //     Backbone.js 1.3.3
 
